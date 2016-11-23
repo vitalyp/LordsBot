f2ae5a7185d28dc13e20093f90999b4b13cc911e
Vitaly Pestov <ariekdev@gmail.com>
2016-11-23 12:07:51 +0200

    buy logo pass

--- lords.rb
+++ lords.rb
@@ -1,5 +1,24 @@
+# coding: utf-8
+
+#
+#   OY_BRO 2016
+#
+
 require 'win32/screenshot'
 require "rautomation"
+require_relative "ui_mapper"
+
+# require 'dl'
+# require 'dl/import'
+# BUTTONS_OK = 0
+# BUTTONS_OKCANCEL = 1
+# BUTTONS_ABORTRETRYIGNORE = 2
+# BUTTONS_YESNO = 4
+# require "Win32API"
+# def msg_box text, title
+#   api = Win32API.new('user32', 'MessageBox',['L', 'P', 'P', 'L'],'I')
+#   api.call(0, text, title, BUTTONS_OK)
+# end
 
 
 
@@ -23,7 +42,7 @@ class LordBot
   end
 
   def makeScreenShot
-    Win32::Screenshot::Take.of(:window, title: @win_name).write(SCREENSHOT_FILENAME)
+    Win32::Screenshot::Take.of(:window, title: @win_name).write!(SCREENSHOT_FILENAME)
   end
   
   def click x, y
@@ -41,15 +60,32 @@ class LordBot
     puts "width: #{@win_width}"
     puts "height: #{@win_height}"
     puts "---- finish information ----"
-
   end
-
 end
 
+class UI_Navigator
+  attr_accessor :bot
 
+  def initialize bot
+    @bot = bot
+  end
+
+  # close logo screen
+  def logo_pass
+    btn_coords = UI_Mapper::BuyLogoPage.CloseBtnCoords
+    @bot.click btn_coords[:left], btn_coords[:top]
+  end
+end
+
+##################################
 
 bot = LordBot.new
 exit unless bot.attach /Droid4X/
 bot.click 200, 200
 bot.makeScreenShot
 bot.printWinInfo
+
+ui = UI_Navigator.new(bot)
+ui.logo_pass
+
+#################################
---
+++ ui_mapper.rb
@@ -0,0 +1,14 @@
+GAME_SCREEN_WIDTH = 1280
+GAME_SCREEN_HEIGHT = 720
+
+BUY_LOGO_PAGE_CLOSE_BTN_COORDS = {left: 1240, top: 80}
+
+class UI_Mapper
+
+  class BuyLogoPage    
+    def self.CloseBtnCoords
+      BUY_LOGO_PAGE_CLOSE_BTN_COORDS
+    end  
+  end
+
+end
